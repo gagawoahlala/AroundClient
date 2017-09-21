7b48207d27480fa898166b10f67800badebf0202
Yikun Huang <yih078@ucsd.edu>
2017-09-20 19:31:33 -0700

    Initial Commit

---
+++ AroundClient/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AroundClient
+//
+//  Created by Yikun Huang on 9/20/17.
+//  Copyright © 2017 Yikun Huang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AroundClient/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AroundClient
+//
+//  Created by Yikun Huang on 9/20/17.
+//  Copyright © 2017 Yikun Huang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
